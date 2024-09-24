d1a2fb5fc4e362ec01326b83acfc77b8919794dd
Perseusx99007 <ratulsaha99@gmail.com>
2024-09-25 03:53:08 +0530

    Setting up URLs

---
+++ hello/urls.py
@@ -0,0 +1,12 @@
+#Creating a urls.py file for the Django app (Hello)
+
+from django.urls import path                    #Importing path from django.urls
+
+from . import views                             #Importing views from the current directory
+
+#Defining the url pattern for the index view. When the root / default URL is requested, the index view will be called. The name parameter is used to refer to this URL pattern in other parts of the application. Here "views" refers to the views.py file in the same directory and "index" refers to the index function in that file.
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("perseus", views.perseus, name='perseus')
+]
